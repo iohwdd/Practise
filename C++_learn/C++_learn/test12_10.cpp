689351f9da324c0d835da857cfd5d4c0dc5ba1d7
iohwdd <2023321332@qq.comgit config --global user.email 2023321332@qq.com>
2022-12-10 22:34:57 +0800

    practise

---
+++ C++_learn/C++_learn/test12_10.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	//float a = 1.244454554f;
+	//cout << a<<"d" << endl;//默认显示6位
+	//char ch = 'a';
+	//cout << "aaaabbbbb\n\t1\\" << endl;// \表示要输出特殊字符
+	//string ch = "nihao";//字符串数据类型
+	//cout << ch << endl;
+	//bool ac = false;
+	//cout << ac << sizeof(bool) << endl;//bool一个字节，只有true与false两个值，分别表示1和0；
+	//string str = "hello";
+	//cout << str << endl;
+	//cin >> str;
+	//cout << str << endl;
+	//bool ac;//字母都为假
+	//cin >> ac;
+	//cout << ac << endl;
+	//int a = -1;
+	//cout << -a << endl;
+	//cout << 1 / 2.0f<< endl;
+	//int a = 1;
+	//int b = 2;
+	//if (!(a == b))//逻辑运算符(返回真假) 非 
+	//	cout << "2" << endl;
+	
+	return 0;
+}
